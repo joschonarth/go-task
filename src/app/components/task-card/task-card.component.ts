8939e410e7aa55850cf8d6199f75b6c4773c2c01
joschonarth <joschonarth@gmail.com>
2026-07-03 22:19:20 -0300

    feat: create method to update task name and description

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, Input } from '@angular/core';
 import { ModalControllerService } from '../../services/modal-controller.service';
 import { ITask } from '../../interfaces/task.interface';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-card',
@@ -12,15 +13,23 @@ export class TaskCardComponent {
   @Input({ required: true }) task!: ITask;
 
   private readonly _modalControllerService = inject(ModalControllerService);
+  private readonly _taskService = inject(TaskService);
 
   openEditTaskModal() {
     const dialogRef = this._modalControllerService.openEditTaskModal({
-      name: 'Nome tarefa',
-      description: 'Descrição tarefa',
+      name: this.task.name,
+      description: this.task.description,
     });
 
     dialogRef.closed.subscribe((taskForm) => {
-      console.log('Tarefa alterada: ', taskForm);
+      if (taskForm) {
+        this._taskService.updateTaskNameAndDescription(
+          this.task.id,
+          this.task.status,
+          taskForm.name,
+          taskForm.description,
+        );
+      }
     });
   }
 }
